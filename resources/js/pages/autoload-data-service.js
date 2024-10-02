e1bc5e452723d3ed28f8b9d64cbafa6bce3eb5c5
Chúc Quân <quanca@imap.edu.vn>
2024-10-02 10:44:48 +0700

    update js ticket

--- resources/js/pages/autoload-data-service.js
+++ resources/js/pages/autoload-data-service.js
@@ -516,6 +516,13 @@ const AutoloadDataService = (function () {
             query: ["topic_id", "status", "_id"],
             version: 2,
         },
+        callcenter_ticket_process: {
+            url: window.API_SERVICE_URL_V2 + "/call-center/ticket-proccess",
+            formated: "$(name)",
+            id: "_id",
+            query: ["topic_id", "_id"],
+            version: 2,
+        },
         "core-service": {
             url: window.API_SERVICE_URL_V2 + "/core/services",
             formated: "$(name)",
@@ -1329,6 +1336,14 @@ const AutoloadDataService = (function () {
         {
             url: window.API_SERVICE_URL_V2 + "/call-center/ticket-topics",
             dom: ".em-ticket-topic",
+            attr: "data-id",
+            formated: "$(name)",
+            fk: "_id",
+            version: 2,
+        },
+        {
+            url: window.API_SERVICE_URL_V2 + "/call-center/ticket-proccess",
+            dom: ".em-ticket-proccess",
             attr: "data-id",
             formated: "$(name)",
             fk: "_id",
