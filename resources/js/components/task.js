9f044a3023937246bbebb4613f2e2e86fb17d78a
Chúc Quân <quanca@imap.edu.vn>
2025-04-28 17:42:37 +0700

    get unit detail

--- resources/js/components/task.js
+++ resources/js/components/task.js
@@ -4,6 +4,11 @@ module.exports = (function(){
             var project_id = params.project_id;
             delete(params.project_id);
             helpers.curlgetapi(API_SERVICE_URL_V2 + '/task/projects/' + project_id, params, callback);
+        },
+        getUnitDetail: function(params, callback) {
+            var unit_id = params.unit_id;
+            delete(params.unit_id);
+            helpers.curlgetapi(API_SERVICE_URL_V2 + '/task/units/' + unit_id, params, callback);
         }
     }
 })();
