454d7dae7a2b3bc4d405058225fa69da46db43ff
Chúc Quân <quanca@imap.edu.vn>
2025-06-16 17:39:05 +0700

    add menu test

--- resources/js/app/common_layout.js
+++ resources/js/app/common_layout.js
@@ -120,6 +120,12 @@ $(document).ready(function () {
                         <div class="font-size-sm font-weight-semibold text-uppercase mt-2">Công việc</div>\
                     </a>\
                 </div>',
+        'test': '<div class="col-4">\
+                    <a href="https://erp.ebomb.edu.vn/tests" class="d-block text-body text-center ripple-dark rounded p-3 load_not_ajax">\
+                        <i class="icon-task text-success icon-2x"></i>\
+                        <div class="font-size-sm font-weight-semibold text-uppercase mt-2">Hệ thống test</div>\
+                    </a>\
+                </div>',
     }
     
     $.each(objMenu,function(k,v){
