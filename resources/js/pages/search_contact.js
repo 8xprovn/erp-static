462c26b5b49ff88b4956e17bba398f2bf18b2b80
thang nd <thangnd1@imap.edu.vn>
2025-09-15 14:50:17 +0700

    update

--- resources/js/pages/search_contact.js
+++ resources/js/pages/search_contact.js
@@ -115,6 +115,7 @@ const searchContactService = (function () {
                 </div>
             </div>`;
             $('body').append(htmlModal);
+            $('#'+ajax_search_id).trigger( "MainContentReloaded", [] );
             $(document).off('hidden.bs.modal', '#' + ajax_search_id).on('hidden.bs.modal', '#' + ajax_search_id, function () {
                 document.activeElement.blur();   // clear focus
                 $('body').focus();               // trả focus về body
